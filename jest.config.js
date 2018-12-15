554a2f3fe97b387c320fe2d7f0f110a8da60c4fc
Brian Le <brianle1301@gmail.com>
2018-12-15 23:53:14 +0700

    Rename enzyme config file

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  setupTestFrameworkScriptFile: '<rootDir>/tests/config.js',
+  setupTestFrameworkScriptFile: '<rootDir>/tests/enzyme.config.js',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/.git/'],
 };
