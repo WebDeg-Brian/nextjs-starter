937bdd0b3b39323856a021e7d40b2ebe2ead75fc
Brian Le <brianle1301@gmail.com>
2018-12-22 06:14:13 +0700

    Improve page test

--- tests/page-tests/index.test.js
+++ tests/page-tests/index.test.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Index } from '../../pages';
 import { shallow } from 'enzyme';
 
-const wrapper = shallow(<Index />);
-
 describe('Tests for index page', () => {
   test('Render without crashing', () => {
+    const wrapper = shallow(<Index />);
+
     expect(wrapper.exists()).toBe(true);
   });
 
   test('Contains hello world', () => {
+    const wrapper = shallow(<Index />);
+    
     expect(wrapper.find('div').text()).toBe('Hello World');
   });
 });
